797b6f9b671d24da744f974318f371a17d617b46
Michael Camilleri <mike@inqk.net>
2017-11-14 03:45:08 +0900

    Separate Shakushi module.

---
+++ get_lowe.rb
@@ -0,0 +1,15 @@
+require_relative 'shakushi'
+
+get_lowe = Shakushi::Base.new(
+  title: 'Get Lowe',
+  description: "Zach Lowe's ESPN columns.",
+  parent_url: 'http://filtrates.inqk.net/',
+  local_dir: 'get-lowe',
+  target_url: 'http://www.espn.com/espn/rss/nba/news',
+  filters: [
+    { attribute: 'title', pattern: /^Lowe:/ }
+  ],
+  match_all: false
+)
+
+puts get_lowe.filtered_feed.inspect
