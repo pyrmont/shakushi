d2e885af182f06795534116dd7390d307bebffa8
Michael Camilleri <mike@inqk.net>
2017-11-17 03:46:31 +0900

    Clean up coding style.

--- shakushi.rb
+++ shakushi.rb
@@ -4,25 +4,33 @@ require 'nokogiri'
 
 module Shakushi
   class Base
+    DATA_DIR = 'data'
     FEED_FILENAME = 'feed.xml'
     FILE_EXT = '.item'
     FILE_SEP = '/'
     MAX_ITEMS = 20
-    ATOM_TAGS = { feed: 'feed', entry: 'entry', id: 'id', published: 'published' }
-    RSS_TAGS = { feed: 'channel', entry: 'item', id: 'guid', published: 'pubDate' }
+    ATOM_TAGS = { feed: 'feed', entry: 'entry', id: 'id',
+                  published: 'published' }
+    RSS_TAGS = { feed: 'channel', entry: 'item', id: 'guid',
+                 published: 'pubDate' }
 
-    def initialize(feed_type:, feed_attributes:, parent_url:, content_dir:, target_url:, filters:, match_all: false, item_function:)
-      @t = setup_tag_names feed_type: feed_type
+    def initialize(feed_type:,
+                   feed_attributes:,
+                   parent_url:,
+                   content_dir:,
+                   target_url:,
+                   filters:,
+                   match_all: false,
+                   item_function: nil)
+      @tag = setup_tag_names feed_type: feed_type
       @feed_attributes = feed_attributes
       @parent_url = parent_url
       @content_dir = content_dir
       @data_dir = setup_cache content_dir: @content_dir
-      @feed = build_feed(
-        url: target_url,
-        filters: filters,
-        match_all: match_all,
-        item_function: item_function
-      )
+      @feed = build_feed(url: target_url,
+                         filters: filters,
+                         match_all: match_all,
+                         item_function: item_function)
     end
 
     def output_rss
@@ -33,45 +41,33 @@ module Shakushi
 
     def setup_tag_names(feed_type:)
       tag_names = case feed_type.to_sym
-        when :atom then ATOM_TAGS
-        when :rss then RSS_TAGS
-      end
+                  when :atom then ATOM_TAGS
+                  when :rss then RSS_TAGS
+                  end
     end
 
     def setup_cache(content_dir:)
-      dirname = 'data' + FILE_SEP + content_dir
+      dirname = DATA_DIR + FILE_SEP + content_dir
       Dir.mkdir dirname unless File.directory? dirname
       dirname
     end
 
     def build_feed(url:, filters:, match_all:, item_function:)
-      feed = filter(
-        xml: Nokogiri::XML(open(url)),
-        filters: filters.map { |f|
-          Shakushi::Filter.new(tag: f[:tag], pattern: f[:pattern])
-        },
-        match_all: match_all,
-        item_function: item_function
-      )
+      feed = filter(xml: Nokogiri::XML(open(url)),
+                    filters: filters.map { |f|
+                      Shakushi::Filter.new tag: f[:tag], pattern: f[:pattern]
+                    },
+                    match_all: match_all,
+                    item_function: item_function)
       feed = modify_tags xml: feed
       feed = preserve_items xml: feed
       feed = restore_items xml: feed
     end
 
     def filter(xml:, filters:, match_all:, item_function:)
-      xml.search(@t[:entry])&.each do |item|
-        if match_all
-          keep_it = filters.reduce(nil) do |memo, f|
-            (memo == nil) ? f.keep?(item) : memo && f.keep?(item)
-          end
-        else
-          keep_it = filters.reduce(nil) do |memo, f|
-            (memo == nil) ? f.keep?(item) : memo || f.keep?(item)
-          end
-        end
-
-        if keep_it
-          item_function.call item
+      xml.search(@tag[:entry])&.each do |item|
+        if keep?(item, filters: filters, match_all: match_all)
+          item_function.call item unless item_function == nil
         else
           item.unlink
         end
@@ -79,82 +75,90 @@ module Shakushi
       xml
     end
 
-    def modify_tags(xml:)
-      @feed_attributes[:link] &&= @parent_url + FILE_SEP + @content_dir + FILE_SEP + FEED_FILENAME
-      @feed_attributes.each do |tag, content|
-        if tag == :itunes
-          modify_itunes_tags(xml: xml, tags: content)
-        else
-          modify_others_tags(xml: xml, tag: tag, content: content)
+    def keep?(item, filters:, match_all:)
+      if match_all
+        filters.reduce(nil) do |memo, f|
+          (memo == nil) ? f.keep?(item) : memo && f.keep?(item)
         end
+      else
+        filters.reduce(nil) do |memo, f|
+          (memo == nil) ? f.keep?(item) : memo || f.keep?(item)
+        end
+      end
+    end
+
+    def modify_tags(xml:)
+      link = @parent_url + FILE_SEP + @content_dir + FILE_SEP + FEED_FILENAME
+      @feed_attributes[:link] = link unless @feed_attributes[:link]
+      @feed_attributes.each do |tag, content|
+        xml = if tag == :itunes
+                modify_itunes_tags xml: xml, tags: content
+              else
+                modify_others_tags xml: xml, tag: tag, content: content
+              end
       end
       xml
     end
 
     def modify_itunes_tags(xml:, tags:)
       tags.each do |partial, content|
-        node = find_or_create_node(xml: xml, tag: 'itunes:' + partial.to_s)
+        node = find_or_create_node xml: xml, tag: 'itunes:' + partial.to_s
         if partial == :image
           node['href'] = content
         else
           node.content = content
         end
       end
+      xml
     end
 
     def modify_others_tags(xml:, tag:, content:)
-      node = find_or_create_node(xml: xml, tag: tag)
+      node = find_or_create_node xml: xml, tag: tag
       node.content = content
+      xml
     end
 
-    def find_or_create_node(xml:, tag:, parent_tag: nil)
-      parent_tag = @t[:feed] if parent_tag == nil
-      if result = xml.at_css(tag.to_s)
-        node = result
-      else
-        parent_node = xml.at_css(parent_tag)
-        node = parent_node.add_child(Nokogiri::XML::Node.new tag.to_s, xml)
-      end
+    def find_or_create_node(xml:, tag:)
+      node = if result = xml.at_css(tag.to_s)
+               result
+             else
+               parent_node = xml.at_css @tag[:feed]
+               parent_node.add_child Nokogiri::XML::Node.new(tag.to_s, xml)
+             end
     end
 
     def preserve_items(xml:)
-      xml.search(@t[:entry]).each do |item|
-        date = DateTime.parse item.at_css(@t[:published]).content
+      xml.search(@tag[:entry])&.each do |item|
+        date = DateTime.parse item.at_css(@tag[:published]).content
         time = date.to_time.to_i
-        hash = Digest::MD5.hexdigest item.at_css(@t[:id]).content
-        path = @data_dir + FILE_SEP + date.year.to_s
-        Dir.mkdir path unless File.directory? path
-        filepath = path + FILE_SEP + time.to_s + '-' + hash + FILE_EXT
-        File.open(filepath, 'w') { |file| file.write(item.to_xml) }
+        hash = Digest::MD5.hexdigest item.at_css(@tag[:id]).content
+        dir_path = @data_dir + FILE_SEP + date.year.to_s
+        file_path = dir_path + FILE_SEP + time.to_s + '-' + hash + FILE_EXT
+        Dir.mkdir dir_path unless File.directory? dir_path
+        File.open(file_path, 'w') { |file| file.write(item.to_xml) }
         item.unlink
       end
       xml
     end
 
     def restore_items(xml:)
-      parent_node = xml.at_css(@t[:feed])
-
-      dirs = Dir.entries(@data_dir)
-      .select { |fn| /^\d{4}$/ === fn }
-      .sort
-      .reverse
-
+      dirs = get_entries dir: @data_dir, pattern: /\A\d{4}\z/
       count = 0
+      parent_node = xml.at_css @tag[:feed]
       dirs.each do |d|
         year_dir = @data_dir + FILE_SEP + d
-        Dir.entries(year_dir)
-        .select { |fn| /#{FILE_EXT}$/ === fn }
-        .sort
-        .reverse
-        .each do |fn|
-          fragment_xml = File.read year_dir + FILE_SEP + fn
-          parent_node.add_child fragment_xml
+        filenames = get_entries dir: year_dir, pattern: /#{FILE_EXT}\z/
+        filenames.each do |f|
+          parent_node.add_child File.read(year_dir + FILE_SEP + f)
           break if (count = count + 1) > MAX_ITEMS
         end
         break if count > MAX_ITEMS
       end
-
       xml
+    end
+
+    def get_entries(dir:, pattern:)
+      Dir.entries(dir).select { |e| pattern === e }.sort.reverse
     end
   end
 
