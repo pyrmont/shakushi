dd5734fd48ce376b63093e09c23ba0a44d6cbf86
Michael Camilleri <mike@inqk.net>
2017-12-04 01:56:29 +0900

    Add test frameworks.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'rake', '~>12.3.0'
+gem 'minitest', '~>5.10.3'
+gem 'minitest-reporters', '~>1.1.19'
+
 gem 'nokogiri', '~>1.8.0'
+gem 'shoulda-context', '~>1.2.0'
---
+++ test/test_type_check.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'shoulda/context'
+require 'type_check'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TypeCheckTest < Minitest::Test
+  context "The TypeCheck Parser" do
+    setup do
+      @Parser = TypeCheck::Parser
+    end
+
+    should "complete the syntax check for a class name" do
+      assert_nil @Parser.check_syntax('String')
+    end
+
+    should "complete the syntax check for multiple classes in a class list" do
+      assert_nil @Parser.check_syntax('String|Float')
+      assert_nil @Parser.check_syntax('Array|Hash|Float')
+      assert_nil @Parser.check_syntax('Array|Set|Point|Regexp')
+    end
+
+    should "complete the syntax check for a collection class" do
+      assert_nil @Parser.check_syntax('Array<String>')
+    end
+
+    should "fail the syntax check for a class name ending in <" do
+      assert_raises(SyntaxError) { @Parser.check_syntax('String<') }
+    end
+  end
+end
