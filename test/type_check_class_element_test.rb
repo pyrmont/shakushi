793d95514fd9acf032ccf728ca3a0cdd0dcd2dcf
Michael Camilleri <mike@inqk.net>
2017-12-08 02:04:05 +0900

    Add contexts and subcontexts to tests.

--- test/type_check_class_element_test.rb
+++ test/type_check_class_element_test.rb
@@ -2,48 +2,50 @@ require 'test_helper'
 require 'type_check'
 
 class TypeCheckClassElementTest < Minitest::Test
-  context "The method TypeCheck::ClassElement#initialize" do
-    setup do
-      @valid_name = 'Test'
-    end
-
-    should "return a TypeCheck::ClassElement when initialised correctly" do
-      ce = TypeCheck::ClassElement.new(name: @valid_name)
-      assert_kind_of TypeCheck::ClassElement, ce
-    end
-
-    should "raise an ArgumentError when initialised with an empty string" do
-      invalid_name = ''
-      assert_raises(ArgumentError) do
-        TypeCheck::ClassElement.new(name: invalid_name)
-      end
-    end
-
-    should "raise a TypeError when initialised with wrong typed arguments" do
-      invalid_names = [nil, Object.new, Array]
-      invalid_names.each do |i|
-        assert_raises(TypeError) { TypeCheck::ClassElement.new(name: i) }
+  context "TypeCheck::ClassElement" do
+    context "has an instance method #initialize that" do
+      setup do
+        @valid_name = 'Test'
       end
 
-      invalid_collections = [Object.new, String.new]
-      invalid_collections.each do |i|
-        assert_raises(TypeError) do
-          TypeCheck::ClassElement.new(name: @valid_name, collection: i)
+      should "return a TypeCheck::ClassElement when initialised correctly" do
+        ce = TypeCheck::ClassElement.new(name: @valid_name)
+        assert_kind_of TypeCheck::ClassElement, ce
+      end
+
+      should "raise an ArgumentError when initialised with an empty string" do
+        invalid_name = ''
+        assert_raises(ArgumentError) do
+          TypeCheck::ClassElement.new(name: invalid_name)
+        end
+      end
+
+      should "raise a TypeError when initialised with wrong typed arguments" do
+        invalid_names = [nil, Object.new, Array]
+        invalid_names.each do |i|
+          assert_raises(TypeError) { TypeCheck::ClassElement.new(name: i) }
+        end
+
+        invalid_collections = [Object.new, String.new]
+        invalid_collections.each do |i|
+          assert_raises(TypeError) do
+            TypeCheck::ClassElement.new(name: @valid_name, collection: i)
+          end
         end
       end
     end
-  end
 
-  context "The method TypeCheck::ClassElement#==" do
-    setup do
-      @valid_name = 'Test'
-      @ce = TypeCheck::ClassElement.new(name: @valid_name)
-    end
+    context "has an instance method #== that" do
+      setup do
+        @valid_name = 'Test'
+        @ce = TypeCheck::ClassElement.new(name: @valid_name)
+      end
 
-    should "raise a TypeError when the object to be compared is wrong type" do
-      invalid_comparisons = [nil, Object.new, Array.new]
-      invalid_comparisons.each do |i|
-        assert_raises(TypeError) { @ce == i }
+      should "raise a TypeError when the object to be compared is wrong type" do
+        invalid_comparisons = [nil, Object.new, Array.new]
+        invalid_comparisons.each do |i|
+          assert_raises(TypeError) { @ce == i }
+        end
       end
     end
   end
--- test/type_check_parser_test.rb
+++ test/type_check_parser_test.rb
@@ -2,71 +2,68 @@ require 'test_helper'
 require 'type_check'
 
 class TypeCheckParserTest < Minitest::Test
-  @@Parser = TypeCheck::Parser
-  @@valid_inputs = ['String',
-                    'Array<String>',
-                    'String|Float',
-                    'Array|Hash|Float',
-                    'Array|Set|Point|Regexp',
-                    'Array<Array<String>>',
-                    'Array<Array<Array<String>>>',
-                    'String|Array<Integer>',
-                    'Array<Integer>|String',
-                    'String|Integer|Array<Hash<Object>>',
-                    'String|Array<String|Integer>|Object',
-                    'Boolean|Array<String|Hash<Point>|Array<String>>']
-
-  context "The method TypeCheck::Parser.check_syntax" do
+  context "TypeCheck:Parser" do
     setup do
-      @Parser = @@Parser
-      @valid_inputs = @@valid_inputs
-      @invalid_strings = ['',
-                         '|',
-                         '<',
-                         '>',
-                         '|String',
-                         'String|',
-                         '<Array',
-                         '>Array',
-                         'Array<>',
-                         'String<',
-                         'String>',
-                         'Integer<<',
-                         'Array<Array<',
-                         'Array<Array<String>']
-      @invalid_nonstrings = [nil,
-                             Object.new,
-                             Array.new]
+      @Parser = TypeCheck::Parser
+      @valid_inputs = ['String',
+                       'Array<String>',
+                       'String|Float',
+                       'Array|Hash|Float',
+                       'Array|Set|Point|Regexp',
+                       'Array<Array<String>>',
+                       'Array<Array<Array<String>>>',
+                       'String|Array<Integer>',
+                       'Array<Integer>|String',
+                       'String|Integer|Array<Hash<Object>>',
+                       'String|Array<String|Integer>|Object',
+                       'Boolean|Array<String|Hash<Point>|Array<String>>']
     end
 
-    should "return nil for valid inputs" do
-      @valid_inputs.each do |v|
-        assert_nil @Parser.check_syntax(v)
+    context "has a class method .check_syntax that" do
+      setup do
+        @invalid_strings = ['',
+                           '|',
+                           '<',
+                           '>',
+                           '|String',
+                           'String|',
+                           '<Array',
+                           '>Array',
+                           'Array<>',
+                           'String<',
+                           'String>',
+                           'Integer<<',
+                           'Array<Array<',
+                           'Array<Array<String>']
+        @invalid_nonstrings = [nil,
+                               Object.new,
+                               Array.new]
+      end
+
+      should "return nil for valid inputs" do
+        @valid_inputs.each do |v|
+          assert_nil @Parser.check_syntax(v)
+        end
+      end
+
+      should "raise a TypeError for non-string parameters" do
+        @invalid_nonstrings.each do |i|
+          assert_raises(TypeError) { @Parser.check_syntax(i) }
+        end
+      end
+
+      should "raise a SyntaxError for invalid strings" do
+        @invalid_strings.each do |i|
+          assert_raises(SyntaxError) { @Parser.check_syntax(i) }
+        end
       end
     end
 
-    should "raise a TypeError for non-string parameters" do
-      @invalid_nonstrings.each do |i|
-        assert_raises(TypeError) { @Parser.check_syntax(i) }
-      end
-    end
-
-    should "raise a SyntaxError for invalid strings" do
-      @invalid_strings.each do |i|
-        assert_raises(SyntaxError) { @Parser.check_syntax(i) }
-      end
-    end
-  end
-
-  context "The method TypeCheck::Parser.parse" do
-    setup do
-      @Parser = @@Parser
-      @valid_inputs = @@valid_inputs
-    end
-
-    should "return an array of TypeCheck::ClassElement for valid inputs" do
-      @valid_inputs.each do |v|
-        assert_equal v, TypeCheckParserTest.reverse_parse(@Parser.parse(v))
+    context "has a class method .parse that" do
+      should "return an array of TypeCheck::ClassElement for valid inputs" do
+        @valid_inputs.each do |v|
+          assert_equal v, TypeCheckParserTest.reverse_parse(@Parser.parse(v))
+        end
       end
     end
   end
