ad75d26c0ddd88e2d645beee3287e28050504271
Michael Camilleri <mike@inqk.net>
2017-12-07 12:58:01 +0900

    Add and refine tests for TypeCheck::Parser.

--- test/type_check_parser_test.rb
+++ test/type_check_parser_test.rb
@@ -7,22 +7,33 @@ require 'type_check'
 Minitest::Reporters.use!
 
 class TypeCheckParserTest < Minitest::Test
-  context "The method TypeCheck::Parser#check_syntax" do
+  @@Parser = TypeCheck::Parser
+  @@valid_inputs = ['String',
+                    'Array<String>',
+                    'String|Float',
+                    'Array|Hash|Float',
+                    'Array|Set|Point|Regexp',
+                    'Array<Array<String>>',
+                    'Array<Array<Array<String>>>',
+                    'String|Array<Integer>',
+                    'Array<Integer>|String',
+                    'String|Integer|Array<Hash<Object>>',
+                    'String|Array<String|Integer>|Object',
+                    'Boolean|Array<String|Hash<Point>|Array<String>>']
+
+  context "The object TypeCheck::Parser" do
     setup do
-      @Parser = TypeCheck::Parser
-      @valid_inputs = ['String',
-                       'Array<String>',
-                       'String|Float',
-                       'Array|Hash|Float',
-                       'Array|Set|Point|Regexp',
-                       'Array<Array<String>>',
-                       'Array<Array<Array<String>>>',
-                       'String|Array<Integer>',
-                       'Array<Integer>|String',
-                       'String|Integer|Array<Hash<Object>>',
-                       'String|Array<String|Integer>|Object',
-                       'Boolean|Array<String|Hash<Point>|Array<String>>']
-      @invalid_inputs = ['String<',
+      @Parser = @@Parser
+      @valid_inputs = @@valid_inputs
+      @invalid_inputs = ['',
+                         '|',
+                         '<',
+                         '>',
+                         '|String',
+                         'String|',
+                         '<Array',
+                         '>Array',
+                         'String<',
                          'Integer<<',
                          'Array<Array<',
                          'Array<Array<String>']
@@ -34,7 +45,7 @@ class TypeCheckParserTest < Minitest::Test
       end
     end
 
-    should "raise syntax error for invalid inputs" do
+    should "raise a SyntaxError for invalid inputs" do
       @invalid_inputs.each do |i|
         assert_raises(SyntaxError) { @Parser.check_syntax(i) }
       end
@@ -43,27 +54,27 @@ class TypeCheckParserTest < Minitest::Test
 
   context "The method TypeCheck::Parser#parse" do
     setup do
-      @Parser = TypeCheck::Parser
-      @ClassElement = TypeCheck::ClassElement
+      @Parser = @@Parser
+      @valid_inputs = @@valid_inputs
     end
 
-    should "complete for a class name" do
-      inputs = ['String']
-      outputs = [[@ClassElement.new(name: 'String')]]
-      inputs.each_with_index do |input, i|
-        assert_equal @Parser.parse(input), outputs[i]
+    should "return an array of TypeCheck::ClassElement for valid inputs" do
+      @valid_inputs.each do |v|
+        assert_equal v, TypeCheckParserTest.reverse_parse(@Parser.parse(v))
       end
     end
+  end
 
-    should "complete for a collection class" do
-      inputs = ['Array<String>']
-      outputs = [[@ClassElement.new(
-                    name: 'Array',
-                    collection: [@ClassElement.new(name: 'String')]
-                  )]]
-      inputs.each_with_index do |input, i|
-        assert_equal @Parser.parse(input), outputs[i]
-      end
+  def self.reverse_parse(array)
+    array.reduce(nil) do |memo, a|
+      memo = (memo.nil?) ? '' : memo + '|'
+      memo = memo + a.name
+      memo = if a.collection.is_a?(Array)
+               inner = TypeCheckParserTest.reverse_parse(a.collection)
+               memo + '<' + inner + '>'
+             else
+               memo
+             end
     end
   end
 end
