15c0f53a046a66758fb11b0667b71c456eba3774
Michael Camilleri <mike@inqk.net>
2017-12-09 00:03:40 +0900

    Add more tests.

--- test/type_check_class_element_test.rb
+++ test/type_check_class_element_test.rb
@@ -38,7 +38,13 @@ class TypeCheckClassElementTest < Minitest::Test
     context "has an instance method #== that" do
       setup do
         @valid_name = 'Test'
+        @other_valid_name = 'Test 2'
         @ce = TypeCheck::ClassElement.new(name: @valid_name)
+      end
+
+      should "return true or false for valid input" do
+        comp = TypeCheck::ClassElement.new(name: @other_valid_name)
+        assert_equal (@ce == comp), false
       end
 
       should "raise a TypeError when the object to be compared is wrong type" do
