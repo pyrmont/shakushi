4744905f2aa8953d1c8b46f001226cc18570501a
Michael Camilleri <mike@inqk.net>
2017-12-12 15:35:21 +0900

    Refactor counters into SyntaxState object.

--- lib/type_check.rb
+++ lib/type_check.rb
@@ -76,117 +76,140 @@ module TypeCheck
       msg = "The string to be checked was empty."
       raise SyntaxError, msg if str.empty?
 
-      counterpart = { lab: '>',
-                      lpr: ')',
-                      constraint: ': or #' }
-
-      state = TypeCheck::Parser::SyntaxState.new(:bar, :lab, :rab, :lpr, :rpr,
-                                                 :hsh, :cln, :cma, :spc, :oth,
-                                                 :end)
-      state.allow(:oth)
-
-      count = { lab: 0, lpr: 0, hsh: 0, constraint: 0 }
+      status_array = [ :bar, :lab, :rab, :lpr, :rpr,:hsh, :cln, :cma, :spc,
+                       :oth, :end ]
+      counter_array = [ [ :angle, :paren, :const ],
+                        { angle: '>', paren: ')', const: ":' or '#" } ]
+      state = TypeCheck::Parser::SyntaxState.new(status_array, counter_array)
 
       i = 0
       chars = str.chars
       str_length = chars.size
 
+      state.allow(:oth)
+
       while (i < str_length)
         msg = "The string '#{str}' has an error here: #{str[0, i+1]}"
         case chars[i]
         when '|' # bar
-          conditions = [ state.allow?(:bar) ]
+          conditions = [ state.allowed?(:bar) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :oth ]
         when '<' # lab
-          conditions = [ state.allow?(:lab) ]
+          conditions = [ state.allowed?(:lab) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :oth ]
-          count[:lab] = count[:lab] + 1
+          state.increment(:angle)
         when '>' # rab
-          conditions = [ state.allow?(:rab), count[:lab] > 0 ]
+          conditions = [ state.allowed?(:rab), state.gtz?(:angle) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :bar, :rab, :lpr, :end ]
-          count[:lab] = count[:lab] - 1
+          state.decrement(:angle)
         when '(' # lpr
-          conditions = [ state.allow?(:lpr) || count[:hsh] == 1 ]
+          conditions = [ state.allowed?(:lpr) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :hsh, :oth ]
-          count[:lpr] = count[:lpr] + 1
-          count[:constraint] = count[:constraint] + 1
+          state.increment(:paren)
+          state.increment(:const)
         when ')' # rpr
-          conditions = [ state.allow?(:rpr), count[:lpr] > 0 ]
+          conditions = [ state.allowed?(:rpr), state.gtz?(:paren) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :bar, :end ]
-          count[:lpr] = count[:lpr] - 1
+          state.decrement(:paren)
         when '#' # hsh
-          conditions = [ state.allow?(:hsh) ]
+          conditions = [ state.allowed?(:hsh) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :oth ]
-          count[:constraint] = count[:constraint] - 1
-          count[:hsh] = count[:hsh] + 1
+          state.decrement(:const)
         when ':' # cln
-          conditions = [ state.allow?(:cln) ]
+          conditions = [ state.allowed?(:cln) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :spc ]
-          count[:constraint] = count[:constraint] - 1
+          state.decrement(:const)
         when ',' # cma
-          conditions = [ state.allow?(:cma) ]
+          conditions = [ state.allowed?(:cma) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :spc ]
-          count[:constraint] = count[:constraint] + 1
+          state.increment(:const)
         when ' ' # spc
-          conditions = [ state.allow?(:spc) ]
+          conditions = [ state.allowed?(:spc) ]
           raise SyntaxError, msg unless conditions.all?
           state.prohibit_all except: [ :oth ]
         else # oth
-          conditions = [ state.allow?(:oth) ]
+          conditions = [ state.allowed?(:oth) ]
           raise SyntaxError, msg unless conditions.all?
           state.allow_all except: [ :hsh, :spc ]
         end
-
         i += 1
       end
       msg_end = "The string '#{str}' ends with an illegal character."
-      raise SyntaxError, msg_end unless state.allow?(:end)
+      raise SyntaxError, msg_end unless state.allowed?(:end)
 
-      char = counterpart[count.find { |k,v| v != 0 }&.first]
-      msg_bal = "The string '#{str}' is missing a #{char}."
-      raise SyntaxError, msg_bal unless count.all? { |k,v| v == 0 }
+      missing = state.unbalanced
+      msg_bal = "The string '#{str}' is missing a '#{missing.first}'."
+      raise SyntaxError, msg_bal unless missing.size == 0
     end
 
     class SyntaxState
-      def initialize(*state_names)
-        @status = Hash[state_names.map {|s| [s, :prohibited]}]
+      def initialize(state_names, counter_names_and_closers)
+        @status = Hash[state_names.map { |s| [s, :prohibited] }]
+        @counter = Hash[counter_names_and_closers[0].map { |c| [c, 0] }]
+        @closers = counter_names_and_closers[1]
       end
 
       def allow(key)
         @status[key] = :allowed
       end
 
-      def allow?(status)
+      def allow_all(except: [])
+        set_all :allowed, except: { exceptions: except, status: :prohibited }
+      end
+
+      def allowed?(status)
         @status[status] == :allowed
       end
 
-      def allow_all(except: [])
-        set_all :allowed, except: { exceptions: except, status: :prohibited }
+      def count(key)
+        @counter[key]
+      end
+
+      def decrement(key)
+        @counter[key] -= 1
+      end
+
+      def increment(key)
+        @counter[key] += 1
+      end
+
+      def gtz?(key)
+        @counter[key] > 0
+      end
+
+      def ltz?(key)
+        @counter[key] < 0
       end
 
       def prohibit(key)
         @status[key] = :prohibited
       end
 
-      def prohibit?(status)
-        @status[status] == :prohibited
-      end
-
       def prohibit_all(except: [])
         set_all :prohibited, except: { exceptions: except, status: :allowed }
+      end
+
+      def prohibited?(status)
+        @status[status] == :prohibited
       end
 
       def set_all(status, except: {})
         @status.transform_values! { |v| v = status }
         except[:exceptions].each { |k| @status[k] = except[:status] }
+      end
+
+      def unbalanced()
+        @counter.reduce(Array.new) do |memo, c|
+          (c[1] == 0) ? memo : memo.push(@closers[c[0]])
+        end
       end
     end
   end
