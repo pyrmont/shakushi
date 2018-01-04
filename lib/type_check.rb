e177564b59542f145806df9c068613f9ab13060f
Michael Camilleri <mike@inqk.net>
2018-01-05 01:52:16 +1100

    Minor stylistic changes.

--- lib/type_check.rb
+++ lib/type_check.rb
@@ -18,16 +18,20 @@ module TypeCheck
               msg = "Argument '#{k}' is not defined."
               raise SyntaxError, msg
             end
+
       types = if hit = TypeCheck::Cache[v]
                 hit
               else
                 TypeCheck::Cache[v] = TypeCheck::Parser.parse v
               end
+
       is_match = types.any? { |t| t.match? arg }
+
       unless collect_invalids || is_match
         msg = "Object '#{k}' is #{arg.class.name} but expected #{v}."
         raise TypeError, msg
       end
+
       (is_match) ? memo : memo.push(k)
     end
   end
