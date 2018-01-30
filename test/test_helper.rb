5410800527db4f5ae5f18e7c5cbcb11c67a9cd80
Michael Camilleri <mike@inqk.net>
2018-01-30 09:22:36 +0900

    Change reporter style.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,7 @@ require 'minitest/reporters'
 require 'shoulda/context'
 
 # Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-# Minitest::Reporters.use!
-Minitest::Reporters.use! [
-  Minitest::Reporters::DefaultReporter.new(:color => false)]
+Minitest::Reporters.use!
 
 module ShakushiTestHelper
 end
